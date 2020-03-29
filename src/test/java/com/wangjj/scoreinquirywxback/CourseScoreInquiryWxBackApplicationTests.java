c72dc823902f45c38bb4c13bd8396919508312b5
wangjj <1090086767@qq.com>
2020-03-29 15:40:40 +0800

    test build

--- src/test/java/com/wangjj/scoreinquirywxback/CourseScoreInquiryWxBackApplicationTests.java
+++ src/test/java/com/wangjj/scoreinquirywxback/CourseScoreInquiryWxBackApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
+import java.util.Deque;
 import java.util.List;
 
 @SpringBootTest
