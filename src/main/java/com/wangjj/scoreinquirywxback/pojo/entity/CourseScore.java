47dfe03e9505a9154843aca8748a8872d0681fda
wangjj <1090086767@qq.com>
2020-04-02 16:39:28 +0800

    修复老师登记成绩导入excel失败的bug

--- src/main/java/com/wangjj/scoreinquirywxback/pojo/entity/CourseScore.java
+++ src/main/java/com/wangjj/scoreinquirywxback/pojo/entity/CourseScore.java
@@ -1,5 +1,7 @@
 package com.wangjj.scoreinquirywxback.pojo.entity;
 
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -17,19 +19,25 @@ import javax.persistence.*;
 @Table(name = "t_exam_score")
 @org.hibernate.annotations.Table(appliesTo = "t_exam_score",comment = "考试成绩类")
 public class CourseScore {
+	@ExcelIgnore
 	@Id
 	private Long id; //ID
 	/** 成绩 */
+	@ExcelProperty(value = "成绩")
 	private Integer score ;
 	/**考试科目*/
+	@ExcelIgnore
 	@OneToOne(fetch = FetchType.LAZY)
 	private Course course;
 	@ManyToOne(fetch = FetchType.LAZY)
 /*	private Teacher teacher;
 	@ManyToOne(fetch = FetchType.LAZY)*/
+	@ExcelIgnore
 	private Student student;
+	@ExcelIgnore
 	@ManyToOne(fetch = FetchType.LAZY)
 	private Exam exam;
+	@ExcelProperty(value = "学号")
 	@Transient
 	private Long studentId;
 }
